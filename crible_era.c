3f3b259ddf082b738a56af480ed8f4eab26bd08a
Rayane <sirri.rayane.ma@gmail.com>
2025-11-13 16:28:18 +0100

    V2 Les tables et l'affichage des tables

--- crible_era.c
+++ crible_era.c
@@ -2,35 +2,44 @@
 
 int main()
 {
-    int n, opt = 0;
+    int n;
+    int pas_pre = 0, pre = 0, opt = 0;
 
     do {
         printf("Max numb :\n");
         scanf("%d", &n);
     } while (n>1000000);
 
-    int T_all[n], T_premi[n];
-    for (int i = 2; i <= n; i++)
-    {
-        printf("Nombre : %d :\t", i);
+    int T_premi[n], T_n_premi[n];
+
+    for (int i = 2; i <= n; i++) {
         for (int j = 2; j < i; j++)
         {
-            if (i % j == 0)
-            {
+            if (i % j == 0){
                 opt = 1;
                 break;
             }
         }
 
-        if (opt == 1)
-        {
-            printf("n'est premier.");
+        if (opt == 1) {
+            T_n_premi[pas_pre] = i;
+            pas_pre++;
             opt = 0;
+        } else {
+            T_premi[pre] = i;
+            pre++;
         }
-        else
-        {
-            printf("est premier.");
-        }
-        printf("\n");
     }
+
+    printf("Nombre Premiers \n");
+    for (int k = 0; k<pre; k++)
+        printf("|%d", T_premi[k]);
+        
+    printf("|");
+
+    printf("\n\nNombre non Premiers \n");
+    for (int k = 0; k<pas_pre; k++)
+        printf("|%d", T_n_premi[k]);
+
+    printf("|");
 }
